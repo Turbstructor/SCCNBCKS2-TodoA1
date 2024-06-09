3f3448bdc1250457ef766c4309511f9abf4d7b59
Turbstructor <ycw0201@naver.com>
2024-06-10 06:50:48 +0900

    🎉 Initial commit

---
+++ src/main/kotlin/spartacodingclub/nbcamp/kotlinspring/assignment/todoextended/TodoExtendedApplication.kt
@@ -0,0 +1,11 @@
+package spartacodingclub.nbcamp.kotlinspring.assignment.todoextended
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TodoExtendedApplication
+
+fun main(args: Array<String>) {
+    runApplication<TodoExtendedApplication>(*args)
+}
---
+++ src/test/kotlin/spartacodingclub/nbcamp/kotlinspring/assignment/todoextended/TodoExtendedApplicationTests.kt
@@ -0,0 +1,13 @@
+package spartacodingclub.nbcamp.kotlinspring.assignment.todoextended
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TodoExtendedApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
